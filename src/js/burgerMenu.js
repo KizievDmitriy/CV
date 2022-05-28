ee9b04b45938bed3ba213a0cbaa4de9d5bcecbf5
KizievDmitriy <kizievdmitrii@gmail.com>
2022-05-29 01:35:03 +0300

    add header

---
+++ src/js/burgerMenu.js
@@ -0,0 +1,9 @@
+
+    // const burger = document.getElementById('burger');
+    // const navLinks = document.querySelector('.nav-links');
+
+    // burger.addEventListener('click', () => {
+    // navLinks.classList.toggle('active');
+    // })
+
+
