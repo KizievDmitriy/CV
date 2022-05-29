b1fca7aaeac3136abfd1ed7104b59e01833e69e2
KizievDmitriy <92225148+KizievDmitriy@users.noreply.github.com>
2022-05-29 18:54:02 +0000

    Deploying to gh-pages from @ KizievDmitriy/CV@4b654fdc8706c7f0da2e4dad410a0d922200fdd2 🚀

--- src.8eb6da9b.js
+++ src.8eb6da9b.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/burgerMenu");
 },{"./sass/main.scss":"clu1","./js/burgerMenu":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/CV/src.f20f92a6.js.map
+//# sourceMappingURL=/CV/src.8eb6da9b.js.map
