47eb4652d26be632f41984819f9337b6750cd1d2
KizievDmitriy <kizievdmitrii@gmail.com>
2022-06-02 12:44:10 +0300

    add mob menu

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,9 +1,12 @@
 const btnBurger = document.querySelector('.burger');
+const mobMenu = document.querySelector('.navrr');
 
 btnBurger.addEventListener('click', btnToggle);
 
 function btnToggle() {
-    btnBurger.classList.toggle("toggled")
+    btnBurger.classList.toggle("toggled");
+    mobMenu.classList.toggle('show');
+    
 };
     
 
