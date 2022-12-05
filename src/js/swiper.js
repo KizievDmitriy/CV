fbd769bbc1aea19e1c3c36ab41da5daffdae8b27
KizievDmitriy <kizievdmitrii@gmail.com>
2022-12-05 18:46:47 +0200

    bug fixed

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,23 +1,23 @@
-export  const swiper = new Swiper('.swiper', {
-          // Optional parameters
-          direction: 'horizontal',
-          loop: true,
+export const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-          // If we need pagination
-          pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-            // dynamicBullets: true,
-          },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    // dynamicBullets: true,
+  },
 
-          // Navigation arrows
-          navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          },
-          
-          // autoplay: {
-          // delay: 2000,
-          // disableOnInteraction: false,
-          // },
-       });
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+
+  // autoplay: {
+  // delay: 2000,
+  // disableOnInteraction: false,
+  // },
+})
